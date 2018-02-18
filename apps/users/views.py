9fccba1f36efc9b8220177e9e6d598e2e4fbfd57
Hapslience <761046327@qq.com>
2018-02-18 13:27:46 +0800

    register

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,16 +2,18 @@
 from django.shortcuts import render
 from .models import UserProfile
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
 from django.views.generic import View
 from django.contrib.auth.backends import ModelBackend
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+from django.db.models import Q
 # Create your views here.
 
 
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         try:
-            user = UserProfile.objects.get(username = username)
+            user = UserProfile.objects.get(Q(username = username)|Q(email = username))
             if user.check_password(password):
                 return user
         except Exception as e:
@@ -35,5 +37,28 @@ class LoginView(View):
                 return render(request, 'login.html', {'msg':'用户名或者密码错误！'})
         else:
             return render(request, 'login.html', {'login_form': login_form})
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html', )
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            email = request.POST.get('email', '')
+            if UserProfile.objects.filter(Q(email=email) | Q(username=email)):
+                return render(request, 'register.html', {'msg':'用户已经存在了'})
+            else:
+                password = request.POST.get('password', '')
+                user_profile = UserProfile()
+                user_profile.email = email
+                user_profile.password = make_password(password)
+                user_profile.username = email
+                user_profile.save()
+                return render(request, 'index.html', {})
+
+            return render(request, 'register.html', {'msg': '账号或者密码不符合规则'})
+        return render(request, 'register.html', {'register_form':register_form})
 
 
---
+++ apps/utils/email_send.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+__author__ = 'lx'
+__date__ = ' 下午10:40'
+
+
+def send_register_email(email, send_type = 'register'):
+    pass
+
+
+
+
+
+
+
+
+
